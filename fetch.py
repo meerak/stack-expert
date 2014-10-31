9fddec77861bdaa772a7eee672057172e528e74c
meerak <rhea@lawn-143-215-119-161.lawn.gatech.edu>
2014-10-31 13:25:50 -0400

    refined code
    refined code

--- fetch.py
+++ fetch.py
@@ -2,15 +2,16 @@ import urllib2
 from urllib import urlopen
 import simplejson as json
 import zlib
+import time
 
 flag = True;
 outfile = open('users.txt', 'w')
 
 pagesize=100
 page = 1
-while(page!=2):
+while(flag):
 
-	stackoverflow_url = 'https://api.stackexchange.com/2.2/users?fromdate=1293840000&todate=1412121600&order=asc&sort=reputation&site=stackoverflow&key=<enter key here>&page=' + str(page) + '&pagesize=' + str(pagesize)
+	stackoverflow_url = 'https://api.stackexchange.com/2.2/users?fromdate=1293840000&todate=1412121600&order=asc&sort=reputation&site=stackoverflow&key=64lUgQSpgbBaPuIqMwbyJw((&page=' + str(page) + '&pagesize=' + str(pagesize)
 	url = urllib2.urlopen(stackoverflow_url)
 	decompressed_data=zlib.decompress(url.read(), 16+zlib.MAX_WBITS)
 	#json_data = open('users.txt')
@@ -22,9 +23,14 @@ while(page!=2):
 	 	break
 	page = page+1
 	outfile.write(",")
-	time.sleep(0.5);
-	print page, data['quota_remaining']
+	backoff = 1
+	if 'backoff' in data:
+		backoff  = data['backoff'] + 1;
+	print page, data['quota_remaining'], backoff
+	time.sleep(backoff);
+	
 
 outfile.close();
+
 
   

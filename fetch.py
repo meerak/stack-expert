d3445664626f7717e532880d47e2d8edbd3097c5
meerak <rhea@lawn-143-215-119-161.lawn.gatech.edu>
2014-10-31 19:29:36 -0400

    handled timeout error

--- fetch.py
+++ fetch.py
@@ -5,31 +5,32 @@ import zlib
 import time
 
 flag = True;
-outfile = open('users.txt', 'w')
+outfile = open('users316.txt', 'w')
 
 pagesize=100
-page = 1
+page = 316
 while(flag):
-
-	stackoverflow_url = 'https://api.stackexchange.com/2.2/users?fromdate=1293840000&todate=1412121600&order=asc&sort=reputation&site=stackoverflow&key=64lUgQSpgbBaPuIqMwbyJw((&page=' + str(page) + '&pagesize=' + str(pagesize)
-	url = urllib2.urlopen(stackoverflow_url)
-	decompressed_data=zlib.decompress(url.read(), 16+zlib.MAX_WBITS)
-	#json_data = open('users.txt')
-	data = json.loads(decompressed_data)
-	x = json.dumps(data['items'])
-	outfile.write(x[1:-1])
-	if(data['has_more'] == False):
-	 	flag = False
-	 	break
-	page = page+1
-	outfile.write(",")
-	backoff = 1
-	if 'backoff' in data:
-		backoff  = data['backoff'] + 1;
-	print page, data['quota_remaining'], backoff
-	time.sleep(backoff);
-	
-
+	try:
+		stackoverflow_url = 'https://api.stackexchange.com/2.2/users?fromdate=1293840000&todate=1412121600&order=asc&sort=reputation&site=stackoverflow&key=64lUgQSpgbBaPuIqMwbyJw((&page=' + str(page) + '&pagesize=' + str(pagesize)
+		url = urllib2.urlopen(stackoverflow_url)
+		decompressed_data=zlib.decompress(url.read(), 16+zlib.MAX_WBITS)
+		#json_data = open('users.txt')
+		data = json.loads(decompressed_data)
+		x = json.dumps(data['items'])
+		outfile.write(x[1:-1])
+		if(data['has_more'] == False):
+		 	flag = False
+		 	break
+		page = page+1
+		outfile.write(",")
+		backoff = 1
+		if 'backoff' in data:
+			backoff  = data['backoff'] + 1;
+		print page, data['quota_remaining'], backoff
+		time.sleep(backoff);
+	except urllib2.URLError:
+	    print 'Oops! Timeout Error! Sorry!', page
+		
 outfile.close();
 
 
